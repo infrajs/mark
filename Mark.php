2aa2474fea607e438211db31d5c340caf36d85f4
Anton <a.kiyatkin@itlf.ru>
2016-12-15 08:27:02 +0400

    fix

--- Mark.php
+++ Mark.php
@@ -5,6 +5,7 @@ use infrajs\sequence\Sequence;
 use infrajs\load\Load;
 use infrajs\each\Each;
 use infrajs\path\Path;
+use infrajs\ans\Ans;
 /**
  * Класс обеспечивает негарантированное хранение параметров в экстрокороткой строке из ~2 символов
  * Это работает за счёт сохранения объекта данных в 2х символах, со временем данные этих ~2х символов буду заменены, но нам важно только короткая память
@@ -22,83 +23,99 @@ use infrajs\path\Path;
  */
 class Mark
 {
-	private $sym = ':';
-	private $symeq = '=';
+	public $sym = ':';
+	public $symeq = '=';
 	//Если метка есть а даных нет считаем что метка устарела.
 	//Недопускаем ситуации что метка появилась до появления привязанных к ней данных
 
-	public $old = array();
+	
 	public $add = array();
 	public $isadd = false;
-	public $isold = false;
-	public $isoutdate=null;
-	public $notice = '';//Сообщение о проблеме
-	private $mark = '';
-	private $data = array();
-	private $warrantytime = 0; //Ссылка не перезаписывается в течении 2х месяцев
-	private $prefix = 'env-';
-	private $len = 1; //Хэшмарк стартовая длина
-	private $raise = 4; //Хэшмарк На сколько символов разрешено увеличивать хэш
-	private $note = 3;//Хэшмарк При увеличении на сколько записывается сообщение в лог
+
+	public $notice = '';//Последнее Сообщение о проблеме
+	public $error = '';//Последнее Сообщение о проблеме
+	
+	public $mark = ''; //Если были внесены изменения то тут хранится новая метка
+	public $data = array();
+	public $origmark = ''; //Старая метка для массива данных
+	public $origdata = array();
+	
+	
+	public $restore = false; //Метка что данные были восстановлены
+	public $change = false; //Метка было ли изменение данных
+	public $len = 2; //Хэшмарк стартовая длина
+	public $raise = 6; //Хэшмарк На сколько символов разрешено увеличивать хэш
+	public $dir = '~.marks/'; //Имя дирректорию где хранятся метки
+	public $checkfail = false; //Результат последней проверки данных, были ли ошибки
 	public function getVal()
 	{
-		return $this->val;
+		return $this->mark;
 	}
 	public function getData($newdata = null)
 	{
 		return $this->data;
 	}
-	public function setData($newdata)
+	public function setData($newdata, $change = true)
 	{
-		$this->data=$newdata;
-		$this->mark=$this->makeMark($this->data);
+		$this->data = $this->check($newdata);
+		if($change || $this->checkfail) {
+			$this->mark = $this->makeMark($this->data);
+		} else {
+			$this->mark = $this->origmark;
+		}
 		return $this->mark;
 	}
-	public static $instances=array();
-	public static function getInstance($mark)
+	private $props = array();
+	public function add($name, $fndef, $fncheck)
 	{
-		if (isset(self::$instances[$mark])) {
-			return self::$instances[$mark];
-		}
-		self::$instances[$mark]=new self($mark);
-		return self::$instances[$mark];
+		$this->props[$name] = array(
+			'fndef' => $fndef,
+			'fncheck' => $fncheck
+		);
 	}
-	private function __construct($mark)
+	public function check($data)
 	{
-		$this->warrantytime=60*60*24*60;//60 дней
-		$mark=preg_replace("/:(.+)::\./U", ":$1::$1.", $mark);
-		$r=explode($this->sym, $mark);
-		$this->mark=array_shift($r);
-		if ($this->mark!='') {
-
-			$src=Path::theme('~.marks/'.$this->prefix.$this->mark.'.json');
+		$this->checkfail = false;
+		if (!is_array($data)) $data = array();
+		foreach ($data as $k => $v) {
+			if (!isset($this->props[$k])) unset($data[$k]);
+		}
+		foreach ($this->props as $k => $v) {
+			if (!isset($data[$k]) || !$v['fncheck']($data[$k])) {
+				$this->checkfail = true;
+				$data[$k] = $v['fndef']();
+			}
+		}
+		return $data;
+	}
+	public function setVal($mark)
+	{
+		$mark = preg_replace("/:(.+)::\./U", ":$1::$1.", $mark);
+		$r = explode($this->sym, $mark);
+		$this->restore = false;
+		$this->origmark = array_shift($r);
+		$this->origdata = array();
+		if ($this->origmark != '') {
+			$src=Path::theme($this->dir.$this->origmark.'.json');
 			if ($src) {
 				$data = file_get_contents($src);
 				$data = Load::json_decode($data);
+				$this->restore = true;
 			} else {
 				$data = false;
 			}
 
-			//$data=infra_mem_get($this->prefix.$this->mark);
-
-			if (!$data || !is_array($data['data'])) {
-				$this->mark='';
-			} else {
-				if (!$data['time']) {
-					$data['time']=time();
-				}
-				$this->isold=true;
-
-				$this->isoutdate=(time()>$data['time']+$this->warrantytime);
-
-				$this->old=$data['data'];
+			if ($data && is_array($data['data']) && $data['time']) {
+				$this->origdata = $data['data'];
 			}
 		}
+	
+		$data = $this->origdata;
 		
-		$this->data=$this->old;
-		
-		$add=implode($this->sym, $r);
-		if($add !== ''){
+		$add = implode($this->sym, $r);
+		$this->change = false;
+		if ($add !== ''){
+			$this->change = true;
 			$r = explode($this->sym, $add);
 			$l = sizeof($r);
 			if ($this->sym == $this->symeq) {
@@ -108,24 +125,28 @@ class Mark
 				}
 				for ($i = 0; $i < $l; $i = $i + 2) {
 					if (!$r[$i]) continue;
-					Sequence::set($this->data, Sequence::right($r[$i]), $r[$i+1]);
+					Sequence::set($data, Sequence::right($r[$i]), $r[$i+1]);
 				}
 			} else {
 				for ($i = 0; $i < $l; $i = $i + 1) {
 					if (!$r[$i]) continue;
 					$rr = explode($this->symeq, $r[$i], 2);
 					if (!$rr[0]) continue;
-					Sequence::set($this->data, Sequence::right($rr[0]), $rr[1]);
+					Sequence::set($data, Sequence::right($rr[0]), $rr[1]);
 				}
 			}
 		}
+		$this->setData($data, $this->change);	
+	}
+	public function __construct($dir)
+	{
+		$this->dir = $dir;	
 	}
 	private function rksort(&$data){
 		ksort($data);
 		foreach($data as &$v){
 			if(!is_array($v))continue;
 			if(Each::isAssoc($v)===true) self::rksort($v);
-			//else sort($v);
 		}
 	}
 	private function makeMark($data)
@@ -133,70 +154,46 @@ class Mark
 		
 		if (!$data) return '';
 		self::rksort($data);
-		
-		$key=md5(serialize($data));
-		$that=$this;
+		$key = md5(Load::json_encode($data));
+		$mark = Once::exec($this->dir.$key, function () use ($data, $key) {
+			$raise = $this->raise; //На сколько символов разрешено увеличивать хэш
 
-		$mark=Once::exec($this->prefix.$key, function () use ($data, $that, $key) {
-			$isoutdate=true;
-			$raise=$this->raise; //На сколько символов разрешено увеличивать хэш
-			$note=$this->note;//При увеличении на сколько записывается сообщение в лог
-
-			$len=$this->len-1;
-			while ($isoutdate&&$len<$this->len+$raise) {
+			$len = $this->len-1;
+			while ($len < $this->len + $raise) {
 				$len++;
-				$mark=substr($key, 0, $len);
-				
-				
-				$src=Path::theme('~.marks/'.$that->prefix.$mark.'.json');
+				$mark = substr($key, 0, $len);
+				$src = Path::theme($this->dir.$mark.'.json');
 				if ($src) {
 					$otherdata = file_get_contents($src);
-				
-					$otherdata = Load::json_decode($otherdata,true);	
-					
+					$otherdata = Load::json_decode($otherdata, true);	
 				} else {
 					$otherdata = false;
 				}
 
-				//$otherdata=infra_mem_get($that->prefix.$mark);
-
 				if ($otherdata && is_array($otherdata['data']) && $otherdata['time']) {
-					if ($otherdata['data']==$data) {
-						$isoutdate=false;//Такая метка уже есть и она правильная
-					} else {
-						//Решается судьба старой метки
-						$isoutdate=(time()>$data['time']+$this->warrantytime);
+					if ($otherdata['data'] == $data) {
+						return $mark; //Такая метка уже есть и она правильная
 					}
 				} else {
-					$isoutdate=false;
+					break; //Выходим из цикла и создадим запись о метке
 				}
 			}
 
-			if ($len>=$this->len+$note) {
-				$that->notice='Mark adding to hash '.($len-$this->len).' symbol(s) for save time warranty '.print_r($data, true);
-				error_log($that->notice);
+			if ($len == $this->len + $raise) {
+				//достигнут лимит по длине... 
+				$this->error = 'Mark rewrite actual hashmark';
+				error_log($this->error);
+				$mark = substr($key, 0, $this->len); //перезаписываем первую
 			}
-			if ($isoutdate) {
-				//Все метки актуальны... перезаписываем первую
-				$that->error='Mark rewrite actual hashmark';
-				error_log($that->error);
-				$mark=substr($key, 0, $this->len);
-			}
-
-			$src=Path::theme('~.marks/');
+			
+			$src = Path::theme($this->dir);
 			if (!$src) die('Fatal error no marks dir');
-			$src=$src.$this->prefix.$mark.'.json';
+			$src = $src.$mark.'.json';
 			$data = Load::json_encode(array(
 				'time'=>time(),
 				'data'=>$data
 			));
 			$data = file_put_contents($src, $data);
-			
-
-			//infra_mem_set($that->prefix.$mark, array(
-			//	'time'=>time(),
-			//	'data'=>$data
-			//));
 			
 			return $mark;
 		});
